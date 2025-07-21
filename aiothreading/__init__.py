fa037f632ef86f1dd6dee225b3d0ef247a11d006
Ilya Egorov <0x42005e1f@gmail.com>
2025-07-21 16:04:53 +0400

    make init friendly for type checkers

--- aiothreading/__init__.py
+++ aiothreading/__init__.py
@@ -8,21 +8,23 @@ made for threading coroutines over asyncio.
 
 __authors__ = ["Vizonex", "x42005e1f"]
 
-from .__version__ import __version__
-from .core import Thread, Worker
-from .pool import ThreadPool, ThreadPoolResult
-from .scheduler import RoundRobin, Scheduler
-from .types import PrematureStopException, QueueID, TaskID
-
-__all__ = (
-    "__version__",
-    "Thread",
-    "Worker",
-    "ThreadPool",
-    "ThreadPoolResult",
-    "PrematureStopException",
-    "RoundRobin",
-    "Scheduler",
-    "QueueID",
-    "TaskID",
+from .__version__ import (
+    __version__ as __version__,
+)
+from .core import (
+    Thread as Thread,
+    Worker as Worker,
+)
+from .pool import (
+    ThreadPool as ThreadPool,
+    ThreadPoolResult as ThreadPoolResult,
+)
+from .scheduler import (
+    RoundRobin as RoundRobin,
+    Scheduler as Scheduler,
+)
+from .types import (
+    PrematureStopException as PrematureStopException,
+    QueueID as QueueID,
+    TaskID as TaskID,
 )
